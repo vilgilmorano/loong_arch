49792e8b4b93fe26e6cfd3db7ed9e215e9359ad1
vilgilmorano <1395014868@qq.com>
2025-07-19 02:15:36 +0800

    Delete add.v

--- add.v
+++
@@ -1,9 +0,0 @@
-module addr(
-    input A, B, C,
-    output out1, out2//1.s 2.carry
-    );
-
-assign out1 = ~A & ~B & C | ~A & B & ~C | A & ~B & ~C | A & B & C;
-assign out2 = A & B | A & C | B & C;
-
-endmodule
